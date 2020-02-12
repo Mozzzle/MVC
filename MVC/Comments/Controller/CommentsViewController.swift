1c97e2f1bf3d1540c95e8923a758e8a47acf62ed
Mozzzle <kuzmitsky1999@mail.ru>
2020-02-12 21:42:40 +0300

    Make MVC

---
+++ MVC/Comments/Controller/CommentsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Slava Kuzmitsky on 12.02.2020.
+//  Copyright © 2020 Slava Kuzmitsky. All rights reserved.
+//
+import UIKit
+
+class CommentsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var comments = [Comment]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        CommentNetworkService.getComments { (response) in
+            self.comments = response.comments
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension CommentsViewController: UITableViewDelegate {}
+
+extension CommentsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)  as! CommentCell
+        let comment = comments[indexPath.row]
+        cell.configure(with: comment)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return comments.count
+    }
+}
